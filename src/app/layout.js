d5584056f05550717cce4e0667e62aed18c74f35
Denisio Rodrigues <denisio@ymail.com>
2025-03-03 22:41:01 -0300

    alrerando a linguagem da aplicaçao para portugues br

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body>
         <div className="app-container">
         <Aside />

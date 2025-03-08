e632ab1faabf1210513db27251e2f9194e3df0c8
Denisio Rodrigues <denisio@ymail.com>
2025-03-08 08:25:31 -0300

    Tratando erro ao chamar a api de posts

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,15 @@ import logger from "@/logger";
 import styles from './page.module.css'
 
 async function getAllPosts() {
-  const response = await fetch('http://localhost:3042/posts')
-  if(!response.ok) {
-    logger.error("Ops, algo ocorreu mal")
+  const response = await fetch('http://localhost:3042/posts').catch((error) => {
+    logger.error("Falha na requisição ao servidor." + error)
+  })
+
+  if(!response || !response.ok) {
+    logger.error("Problema ao obter os posts")
+    return []
   }
-  logger.info("Posts carregados com sucesso")
+
   return response.json()
 } 
  
@@ -15,10 +19,10 @@ async function getAllPosts() {
 export default async function Home() {
   const posts = await getAllPosts()
   return (
-    <main className={styles.container}>
+    <main className={styles.grid}>
 
       {
-        posts.map(post => <CardPost post={post} />)
+        posts.map(post => <CardPost key={post.id}  post={post} />)
       }
     </main>
   );

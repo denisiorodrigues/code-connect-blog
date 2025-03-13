a6e9e2f307c713a38467f04aa99d3076162d9eca
Denisio Rodrigues <denisio@ymail.com>
2025-03-13 19:16:38 -0300

    formandato a página de erro

--- src/app/error.js
+++ src/app/error.js
@@ -17,12 +17,12 @@ export default function Error(error) {
 
     return (
         <div className={style.container}>
-       <Image src={banner}/>
-       <Heading>Opa! Ocorreu um erro.</Heading>
-       <p className={style.text}>Não conseguimos carregar a página, volte para seguir navegando.</p>
-       <Link href="/">
-         Voltar ao feed <ArrowBack color='#81FE88'/>
-       </Link>
-     </div>
+            <Image src={banner} />
+            <Heading>Opa! Ocorreu um erro.</Heading>
+            <p className={style.text}>Não conseguimos carregar a página, volte para seguir navegando.</p>
+            <Link href="/">
+                Voltar ao feed <ArrowBack color='#81FE88' />
+            </Link>
+        </div>
     )
 }

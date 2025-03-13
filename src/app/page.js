520a1be27c7641451cbeb1de82463bb46ad7aefe
Denisio Rodrigues <denisio@ymail.com>
2025-03-13 19:33:47 -0300

    paginação de post

--- src/app/page.js
+++ src/app/page.js
@@ -7,13 +7,25 @@ import db from "../../prisma/db";
 async function getAllPosts(page = 1) {
   try{
     
+    const perPage = 4
+    const skip = (page - 1) * perPage
+    const totalItems = await db.post.count()
+    const totalPages = Math.ceil(totalItems / perPage)
+    const next = page < totalPages ? page + 1 : null
+    const prev = page > 1 ? page - 1 : null
+
     const posts = await db.post.findMany({
+      take: perPage,
+      skip,
+      orderBy: {
+        createdAt: 'desc'
+      },  
       include: {
         author: true
       }
     })
     
-    return { data:posts, next: null, prev: null }
+    return { data:posts, next, prev }
 
   } catch (error) {
     logger.error(error)
@@ -23,7 +35,7 @@ async function getAllPosts(page = 1) {
  
 
 export default async function Home({ searchParams }) {
-  const curretPage = searchParams?.page || 1
+  const curretPage = parseInt(searchParams?.page || 1)
   const { data: posts, next, prev } = await getAllPosts(curretPage)
   return (
     <main className={styles.grid}>
